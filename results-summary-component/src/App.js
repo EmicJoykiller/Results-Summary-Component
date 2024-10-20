67416dae9991811cd46bcf778fd2bbe5d7ac7239
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2024-10-20 18:40:56 -0400

    some of the work for section left has been done

--- results-summary-component/src/App.js
+++ results-summary-component/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="section secl">
+          <h1>Your Result</h1>
+          <div className="secl-score">
+            <h1>76</h1>
+            <h2>of 100</h2>
+          </div>
+            <h2>Great</h2>
+            <p>You scored higher than 65% of <br />
+            the people who have taken <br />
+            these tests.</p>
+        </div>
+        <div className="section secr">im 2</div>
+      </div>
     </div>
   );
 }
